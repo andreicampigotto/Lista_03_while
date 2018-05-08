c42fcab76c9844e73e564e63532b66e4d42f961d
andreicampigotto <andreicampigotto@gmail.com>
2018-05-08 15:57:57 -0300

    7
    7 até metade

--- Lista_03_while/Lista_03_while/Exercicio07.cs
+++ Lista_03_while/Lista_03_while/Exercicio07.cs
@@ -9,22 +9,38 @@ namespace Lista_03_while
     {
         public Exercicio07()
         {
+            int quatidadeAtletas = 0;
+            string nomes = " ";
             Console.Write("Digite o nome do atleta: ");
             string nome = Console.ReadLine();
 
-            Console.Write("Digite a idade do atleta: ");
+            Console.Write("Digite a idade: ");
             int idade = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Digite o peso do atleta: ");
+            Console.Write("Digite o peso: ");
             double peso = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Digite o sexo do atleta: ");
+            Console.WriteLine("Digite o sexo: ");
             char sexo = Convert.ToChar(Console.ReadLine().ToLower());
 
-            Console.Write("Digite a altura do atleta: ");
+            Console.Write("Digite a altura: ");
             double altura = Convert.ToDouble(Console.ReadLine());
 
-            //Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de gols marcados: ");
+            int golsMarcados = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de cartões amarelos recebidosa: ");
+            int cartaoAmaelo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de cartões vermelhos: ");
+            int cartaoVermelho = Convert.ToInt32(Console.ReadLine());
+            quatidadeAtletas = quatidadeAtletas + 1;
+
+            while (quatidadeAtletas < 23)
+            {
+               
+            }
+
 
         }
     }
